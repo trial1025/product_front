03198018afebe1f31e38bb56534189f5f3b373c2
Rood <a2973799@gmail.com>
2024-04-10 01:27:33 +0800

    修復我的最愛移除功能

--- src/views/front/CartView.vue
+++ src/views/front/CartView.vue
@@ -6,8 +6,8 @@ VContainer(d-flex style="background-color:;")
     VDivider
     VCol(cols="10")
       VRow
-        VCol(v-for="item in favorite" :key="item._id" cols="3")
-          VCard(height="360px" elevation)
+        VCol(v-for="item in favorite" :key="item._id")
+          VCard(height="360px" width="200px" elevation)
             RouterLink.text-decoration-none(:to="'/products/' + item.product._id" style="color:black")
               VRow
                 VCol(cols="2")
@@ -40,7 +40,7 @@ const favorite = ref([])
 
 const deleteItem = async (id) => {
   try {
-    await apiAuth.delete('/favorite/products/' + id)
+    await apiAuth.delete('/users/favorite/' + id)
     createSnackbar({
       text: '刪除成功',
       showCloseButton: false,
@@ -52,16 +52,28 @@ const deleteItem = async (id) => {
     })
     favorite.value = favorite.value.filter(item => item._id !== id)
   } catch (error) {
-    const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
-    createSnackbar({
-      text,
-      showCloseButton: false,
-      snackbarProps: {
-        timeout: 2000,
-        color: 'red',
-        location: 'bottom'
-      }
-    })
+    if (error.response && error.response.status === 404) {
+      createSnackbar({
+        text: '產品找不到，可能已被刪除',
+        showCloseButton: false,
+        snackbarProps: {
+          timeout: 2000,
+          color: 'red',
+          location: 'bottom'
+        }
+      })
+    } else {
+      const text = error?.response?.data?.message || '發生錯誤，請稍後再試'
+      createSnackbar({
+        text,
+        showCloseButton: false,
+        snackbarProps: {
+          timeout: 2000,
+          color: 'red',
+          location: 'bottom'
+        }
+      })
+    }
   }
 }
 
