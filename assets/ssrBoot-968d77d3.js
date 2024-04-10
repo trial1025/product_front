0e3abe5f4d4ce901358162d6db19415811e75938
trial1025 <a2973799@gmail.com>
2024-04-10 18:20:36 +0000

    Deploying to gh-pages from @ trial1025/product_front@2443ba61a43a357d951e24562ba7c77f97f17b89 🚀

--- assets/ssrBoot-968d77d3.js
+++
@@ -1,1 +0,0 @@
-import{s,A as t,o as e,b1 as n}from"./index-71158f2c.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
